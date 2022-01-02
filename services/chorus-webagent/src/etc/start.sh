f1ad27ce1bf497e8e9d70b31a9f759307b32f475
Nick Ebbutt <nick@logicmint.co.uk>
2022-01-02 01:03:59 +0000

    Log4j2 Upgrade

--- services/chorus-webagent/src/etc/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# Start the jtimeseries-agent
-
-java -Xmx1024M -cp . org.chorusbdd.chorus.tools.webagent.ChorusWebAgent
-
--- services/chorus-webagent/src/test/resources/SimpleFeature.feature
+++
@@ -1,6 +0,0 @@
-Uses: Timers
-
-Feature: Simple Feature
-
-  Scenario: Simple Scenario
-  Given I wait 1 seconds for a step to run
